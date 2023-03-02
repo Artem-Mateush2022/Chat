ed6288008b896c8733081f0ec68f52f290929a65
Artem-Mateush2022 <103309654+Artem-Mateush2022@users.noreply.github.com>
2023-03-02 11:42:21 +0200

    added_react-router-dom

--- src/components/chat/Main/components/ChatItem.jsx
+++ src/components/chat/Main/components/ChatItem.jsx
@@ -7,6 +7,7 @@ import ListItemAvatar from '@mui/material/ListItemAvatar';
 import { Avatar, Typography, Link} from '@mui/material';
 import styled from "styled-components";
 import { Box } from '@mui/system';
+import { NavLink, useParams } from 'react-router-dom';
 
 const LastMessageText = styled(Typography)(({ theme }) => ({
     overflow: 'hidden', 
@@ -17,38 +18,39 @@ const LastMessageText = styled(Typography)(({ theme }) => ({
 
 export function ChatItem({ chatInfo }) {
     return(
-        <Link href={`/${chatInfo.id}`} sx={{textDecoration: 'none', color: 'inherit'}}>
+        <NavLink to={'/roomId'} style={{textDecoration: 'none', color: 'inherit'}}>
             <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper',}}>
-            <ListItem alignItems="flex-start" sx={{cursor: 'pointer'}}>
-                <ListItemAvatar>
-                    <Avatar alt={chatInfo.userName} src={chatInfo.avatar} />
-                </ListItemAvatar>
-                <ListItemText
-                    sx={{fontSize: '50px'}}
-                    primary={
-                        <React.Fragment>
-                            <Box sx={{display: 'flex', justifyContent: 'space-between', alignItems: 'center',}}>
-                                <Typography sx={{overflow: 'hidden', whiteSpace: 'nowrap', textOverflow: 'ellipsis', fontSize: 18, textDecoration: 'none'}}>{chatInfo.userName}</Typography>
-                                <Typography sx={{fontSize: 13, color: 'text.primary',}}>{chatInfo.sendTime}</Typography>
-                            </Box>
-                        </React.Fragment>
-                    }
-                    
-                    secondary={<React.Fragment>
-                        <Typography
-                            sx={{ display: 'inline' }}
-                            component="span"
-                            variant="body2"
-                            color="text.primary"
-                        >
-                        </Typography>
-                        <LastMessageText>
-                            {chatInfo.lastMessage}
-                        </LastMessageText>
-                    </React.Fragment>} />
+                <ListItem alignItems="flex-start" sx={{cursor: 'pointer'}}>
+                    <ListItemAvatar>
+                        <Avatar alt={chatInfo.userName} src={chatInfo.avatar} />
+                    </ListItemAvatar>
+                    <ListItemText
+                        sx={{fontSize: '50px'}}
+                        primary={
+                            <React.Fragment>
+                                <Box sx={{display: 'flex', justifyContent: 'space-between', alignItems: 'center',}}>
+                                    <Typography sx={{overflow: 'hidden', whiteSpace: 'nowrap', textOverflow: 'ellipsis', fontSize: 18, textDecoration: 'none'}}>{chatInfo.userName}</Typography>
+                                    <Typography sx={{fontSize: 13, color: 'text.primary',}}>{chatInfo.sendTime}</Typography>
+                                </Box>
+                            </React.Fragment>
+                        }
+                        
+                        secondary={<React.Fragment>
+                            <Typography
+                                sx={{ display: 'inline' }}
+                                component="span"
+                                variant="body2"
+                                color="text.primary"
+                            >
+                            </Typography>
+                            <LastMessageText>
+                                {chatInfo.lastMessage}
+                            </LastMessageText>
+                        </React.Fragment>}
+                    />
                 </ListItem>
-            <Divider variant="inset" component="li" />
+                <Divider variant="inset" component="li" />
             </List>
-        </Link>
+        </NavLink>
     )
 };
